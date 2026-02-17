df738abd720756fc5f7265452265b2d81ce2b711
Michele Nalli <m.nalli@ikmail.com>
2026-02-17 18:19:03 +0100

    Add git-cola binaries
    Once installed, they will be present under ~/.local/bin as
    recursive symlinks.

--- install.sh
+++ install.sh
@@ -11,5 +11,6 @@ ln -s $(find "$DOTFILES_DIR" -maxdepth 1 -type f -name '.*') ~/
 ln -s "$DOTFILES_DIR"/.config/* ~/.config/
 
 # link .local sub-directories
-ln -s "$DOTFILES_DIR"/.local/src/* ~/.local/src/
+ln -s "$DOTFILES_DIR"/.local/bin/* ~/.local/bin/
 ln -s "$DOTFILES_DIR"/.local/share/* ~/.local/share/
+ln -s "$DOTFILES_DIR"/.local/src/* ~/.local/src/
