42d71b072b8b0293c21dff36879e0c25be522ae1
Michele Nalli <m.nalli@ikmail.com>
2026-02-18 15:32:30 +0100

    Add completions.sh

---
+++ .config/bash/conf.d/completions.sh
@@ -0,0 +1,14 @@
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+complete -C /snap/aws-cli/current/bin/aws_completer aws
+
+eval "$(kubectl completion bash)"
